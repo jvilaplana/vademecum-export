3d79f173f21e221780816d0bc023d548ebe7326f
Jordi Vilaplana <gatasuna@gmail.com>
2017-04-27 12:18:13 +0200

    Added result files.

--- README.md
+++ README.md
@@ -1,2 +1,43 @@
 # vademecum-export
 Web scraper to export drug data into CSV files.
+
+This program scans the [Vademecum.es](http://vademecum.es) website to obtain a list of all the drugs.
+
+If you don't want to run the program by yourself, you [can check the drug list file here](https://github.com/jvilaplana/vademecum-export/blob/master/vademecum.csv).
+
+## Requirements
+To run this program the following software is required:
+- git
+- python 2.7
+- pip
+- virtualenv (recommended)
+
+## Install
+To install this program in your computer run the following commands in your terminal:
+```
+git clone https://github.com/jvilaplana/vademecum-export.git
+cd vademecum-export
+virtualenv venv
+source venv/bin/activate
+pip install -r requirements.txt
+deactivate
+```
+Now the program should be ready to be executed.
+
+## Run
+To execute the program run the following commands in your terminal:
+```
+source venv/bin/activate
+python vademecum_export.py
+```
+
+## Analyze results
+To check how many drugs were successfully retrieved run:
+```
+find . -name 'vademecum-*' | xargs wc -l
+```
+
+To combine all the CSV files into a single one run:
+```
+cat vademecum-* > vademecum.csv
+```
--- vademecum_export.py
+++ vademecum_export.py
@@ -7,25 +7,32 @@ import string
 import csv
 import time
 
+# We are going to iterate through all leters (a - z).
 letter_list = string.lowercase[:26]
 
-
+# Each letter has its own page with its drug list.
+# We will be saving a CSV file for each starting letter.
 for letter in letter_list:
     with open('vademecum-' + str(letter) + '.csv', 'wb') as csvfile:
+        # We will be saving the drug code, name and URL.
         fieldnames = ['cod_nacion', 'nombre', 'url']
         writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
 
         print "Going for letter " + str(letter)
         vademecum = "http://vademecum.es/medicamentos-" + letter + "_1"
 
+        # We are faking our user-agent to appear like a regular browser.
+        # This prevents unwanted connection resets.
         headers = {'User-agent' : 'Mozilla/5.0'}
         req = urllib2.Request(vademecum, None, headers)
-        page = urllib2.urlopen(req).read()
+        page = urllib2.urlopen(req, timeout = 3).read()
         soup = BeautifulSoup(page, "html.parser")
 
         drug_list = soup.find('ul', class_='no-bullet')
 
+        i = 0
         for row in drug_list.findAll("li"):
+            i += 1
             link = row.find('a')
             drug_link = 'http://vademecum.es' + str(link['href']).strip().encode('utf8')
             drug_name = link.contents[0].strip().encode('utf8')
@@ -33,20 +40,31 @@ for letter in letter_list:
 
             req = urllib2.Request('http://vademecum.es' + str(link['href']), None, headers)
             page = None
+            # This loop prevents the script from stopping in case of a connection error.
             while True:
                 try:
-                    page = urllib2.urlopen(req).read()
+                    page = urllib2.urlopen(req, timeout = 3).read()
+                # If user is trying to stop the script, we will comply
+                except (KeyboardInterrupt, SystemExit):
+                    raise
+                # If something else happens, we will just ignore it and move forward
                 except:
                     print "\t\t[WARNING] Exception trying to open page!"
                     continue
                 break
-            #page = urllib2.urlopen('http://vademecum.es' + str(link['href']))
+
             drug_soup = BeautifulSoup(page, "html.parser")
             for elem in drug_soup.findAll("strong"):
-                if "digo Nacional:" in elem.contents[0]:
-                    drug_code = elem.parent.find("span").contents[0].strip().encode('utf8')
-                    print "\tGetting drug " + drug_code
-                    writer.writerow({'cod_nacion': drug_code, 'nombre': drug_name, 'url': drug_link})
-
+                try:
+                    if "digo Nacional:" in elem.contents[0]:
+                        drug_code = elem.parent.find("span").contents[0].strip().encode('utf8')
+                        print "\tGetting drug " + drug_code + " (" + letter + "-" + str(i) + ")"
+                        writer.writerow({'cod_nacion': drug_code, 'nombre': drug_name, 'url': drug_link})
+                except (KeyboardInterrupt, SystemExit):
+                    raise
+                    continue
+                except:
+                    print "\t\t[WARNING] Exception trying to get drug_code!"
+                    continue
         print "\tDone!"
     time.sleep(1)
